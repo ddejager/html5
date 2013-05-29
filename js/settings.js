22e4c46d1f821f9804995ad5dc42d02de467c930
ddejager <dennis.de.jager@odfjell.com>
2013-05-29 15:04:57 +0200

    Adjusted version, and added timeout for ajax calls on android (results in 401 now)

--- js/settings.js
+++ js/settings.js
@@ -8,7 +8,9 @@ var devicemodel = '';
 var isonline = true;
 var hasPhoneGap = false;
 var appVersion='';
-
+var ajax_timeout = 5000;
+var http_method = 'GET';
+var ext_auth = true;
 
 // Try catch block, so this call won't result in errors (only ment for mobile/gap use!
 try { document.addEventListener("deviceready", onDeviceReady, false); }
@@ -30,6 +32,10 @@ function onDeviceReady() {
 		document.addEventListener("online", wentOnline, false);
 		document.addEventListener("offline", wentOffline, false);
 		checkConnection();
+		
+		// Added some extra auth methods to get around the Android 401 errors, but not needed for ios, so disabled here.. 
+		if (platform=='IOS') 
+			ext_auth = false;
 	}
 	catch (err) {
 		alert('error on ondeviceready:' + err);
@@ -184,13 +190,15 @@ function generic_ajax_sync(wsurl,data,funct) {
 	var wscall= wsbase + wsurl;
 	$.ajax( {	
 		url:wscall,
-		type:'GET',
+		type:http_method,
 		cache:false,
 		async:true,
 		data:data,
 		username:Decr(localStorage.ws_username),
 		password:Decr(localStorage.ws_password),
 		success:funct,
+		beforeSend:extauth,
+		timeout:ajax_timeout,
 		error: function (request, status, error,exception) {
 			alert('De webservice kon niet worden aangeroepen.\nControleer je verbinding, en/of de gebruikernaam en wachtwoord, en probeer opnieuw\n' +
 				  'url:' + wscall + '\n'+ 
@@ -210,10 +218,12 @@ function generic_ajax(wsurl,data,funct) {
 		wscall = wsbase + wsurl;
 	$.ajax( {	
 		url:wscall,
-		type:'GET',
+		type:http_method,
 		cache:false,
 		async:true,
 		data:data,
+		timeout:ajax_timeout,
+		beforeSend:extauth,
 		username:Decr(localStorage.ws_username),
 		password:Decr(localStorage.ws_password),
 		success:funct,
@@ -237,9 +247,11 @@ function getVersion() {
 	try {
 		$.ajax( {	
 			url:'config.xml',
-			type:'GET',
+			type:http_method,
 			cache:false,
 			async:true,
+			beforeSend:extauth,
+			timeout:ajax_timeout,
 			success:gotVersion,
 			error: function (request, status, error) {},
 			dataType:'xml'
@@ -252,4 +264,143 @@ function gotVersion(xml) {
 	
 	$('#footer').html(getStatusInfo());
 }
+function extauth(xhr) {
+	if (ext_auth) {
+		var un = Base64.encode( Decr(localStorage.ws_username));
+		var up = Base64.encode(Decr(localStorage.ws_password));
+		xhr.setRequestHeader('Authorization', 'Basic ' + un + ":" + up  );
+	}
+}
 
+var Base64 = {
+
+    // private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    // public method for encoding
+    encode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = Base64._utf8_encode(input);
+
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+        }
+
+        return output;
+    },
+
+    // public method for decoding
+    decode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+
+        }
+
+        output = Base64._utf8_decode(output);
+
+        return output;
+
+    },
+
+    // private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+
+        return utftext;
+    },
+
+    // private method for UTF-8 decoding
+    _utf8_decode : function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utftext.length ) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
+}
